a2f67637f09bc10e3b5c90c47d1d8dbfbd9e3cc8
COM <COM@COM-PC>
2020-06-11 15:33:03 +0900

    TestController add

--- src/main/java/com/mycompany/project/controller/TestController.java
+++ src/main/java/com/mycompany/project/controller/TestController.java
@@ -1,5 +1,19 @@
 package com.mycompany.project.controller;
 
 public class TestController {
-
+	public void method1() {
+		
+	}
+	
+	public void method2() {
+		
+	}
+	
+	public void method3() {
+		
+	}
+	
+	public void method4() {
+	
+	}
 }
